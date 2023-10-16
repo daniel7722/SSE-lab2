f44d9382e79ab76491a9645824007e8183eb682e
Daniel Huang <daniel.huang20@imperial.ac.uk>
2023-10-16 16:38:38 +0100

    confuse

--- api/app.py
+++ api/app.py
@@ -7,6 +7,7 @@ def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
     return render_template("hello.html", name= input_name, age = input_age)
+
 @app.route("/")
 def hello_world():
     return render_template("index.html")
