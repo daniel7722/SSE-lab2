d444b039a903a2b16945577dcd92ef590aab312c
Rajveer Sehmbi <rajveer.sehmbi18@imperial.ac.uk>
2023-10-23 16:28:06 +0100

    flake8 makes me angry

--- api/app_test.py
+++ api/app_test.py
@@ -3,8 +3,8 @@ from app import process_query
 
 def test_knows_about_dinosaurs():
     assert process_query(
-			"dinosaurs"
-			) == "Dinosaurs ruled the Earth 200 milion years ago"
+            "dinosaurs"
+            ) == "Dinosaurs ruled the Earth 200 milion years ago"
 
 
 def test_does_not_know_about_asteroids():
