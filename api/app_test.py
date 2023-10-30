73ee68de1e3456b47a89123c26b768c5d4299d22
Rajveer Sehmbi <rajveer.sehmbi18@imperial.ac.uk>
2023-10-30 15:33:45 +0000

    LArgest 98

--- api/app_test.py
+++ api/app_test.py
@@ -26,3 +26,7 @@ def test_add_number():
 
 def test_knows_team_name():
     assert process_query("What is your name?") == "DR"
+
+
+def test_knows_number_largest_98():
+    assert process_query("	Which of the following numbers is the largest: 98, 21, 41?") == '98'
