cfa37169480bf1a2b740396960a21fa5717931d4
Daniel Huang <daniel.huang20@imperial.ac.uk>
2023-10-30 15:21:49 +0000

    flake8ahhh

--- api/app.py
+++ api/app.py
@@ -40,7 +40,7 @@ def process_query(q):
     elif q == "dadjoke":
         return "DO NOT TELL ME DAD JOKE"
 
-    elif q == "What is your name?": 
+    elif q == "What is your name?":
         return "DR"
 
 
